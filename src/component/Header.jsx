6b31dbc08edb3436ba24246b30578d83447ec052
mrmai1011 <chihung1011@gmail.com>
2025-05-06 17:39:51 +0700

    up

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,10 @@
 import "../App.scss"
-import logo from "/logo.png"
+import logo from "../assets/logo.png"
 export default function Header()
 {
     return(
         <div className="header">
-            <img className="h-logo" src="/logo.png"></img>
+            <img className="h-logo" src={logo}></img>
         </div>
     );
 }
