b00c290e0ed4bd5970fbe596ef77b0a48aaa3857
mrmai1011 <chihung1011@gmail.com>
2025-05-06 17:46:44 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.scss'
 import Header from './component/Header'
 import Footer from './component/Footer'
